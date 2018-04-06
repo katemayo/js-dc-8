4d92a11fc068b576c452ca802362df98f3f12a2c
Katie Mayo <katie.jill@outlook.com>
2018-04-05 20:11:59 -0400

    practice exercises

--- 03-control_flow/exercise/02_switch_exercise.js
+++ 03-control_flow/exercise/02_switch_exercise.js
@@ -17,3 +17,22 @@ if (grade === 'A') {
 }
 
 // Use a switch statement to re-create the above if/else logic. Which solution is easier?
+switch (grade) {
+  case 'A':
+    console.log('Awesome job')
+    break;
+  case 'B':
+    console.log('Good job')
+    break;
+  case 'C':
+    console.log('Okay job')
+    break;
+  case 'D':
+    console.log('Not so good job')
+    break;
+  case 'F':
+    console.log('Bad job')
+    break;
+  default:
+    console.log('Unexpected grade value entered')
+}
