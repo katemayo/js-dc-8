b25ae07b694d8893eb7c88bebf8b2cc6ac6560b6
Katie Mayo <katie.jill@outlook.com>
2018-04-05 20:38:58 -0400

    homework

--- 03-control_flow/homework/bottles.js
+++ 03-control_flow/homework/bottles.js
@@ -3,31 +3,31 @@
 
 
 // function pickNumber() {
-  
+
 // }
 
 let bottles = 99;
-  var person = prompt("Please enter a number", "a numeral greater than one");
-  document.getElementById("demo").innerHTML =
-    if (bottles.isNaN()) {
-      document.body.append('number please')
-    } else {
-      for (bottles; bottles > 0; bottles--) {
-        // most likely case for numbers 3-99
-        if (bottles > 2) {
-          document.body.append(`${bottles} bottles of beer on the wall, ${bottles} bottles of beer. Take one down and pass it around, ${bottles - 1} bottles of beer on the wall.\n`)
-        }
-        // case when one less bottle is no longer plural ie 2
-        else if (bottles > 1) {
-          document.body.append(`${bottles} bottles of beer on the wall, ${bottles} bottles of beer. Take one down and pass it around, ${bottles - 1} bottle of beer on the wall.\n`)
-        }
-        // case with 1 bottle
-        else if (bottles > 0) {
-          document.body.append(`${bottles} bottle of beer on the wall, ${bottles} bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.\n`)
-        }
-        // 0 bottles
-        else {
-          document.body.append(`No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall\n`)
-        }
-      }
-    }
+// var person = prompt("Please enter a number", "a numeral greater than one");
+// document.getElementById("demo").innerHTML =
+// if (bottles.isNaN()) {
+//document.body.append('number please')
+// } else {
+for (bottles; bottles > 0; bottles--) {
+  // most likely case for numbers 3-99
+  if (bottles > 2) {
+    console.log(`${bottles} bottles of beer on the wall, ${bottles} bottles of beer. Take one down and pass it around, ${bottles - 1} bottles of beer on the wall.\n`)
+  }
+  // case when one less bottle is no longer plural ie 2
+  else if (bottles > 1) {
+    console.log(`${bottles} bottles of beer on the wall, ${bottles} bottles of beer. Take one down and pass it around, ${bottles - 1} bottle of beer on the wall.\n`)
+  }
+  // case with 1 bottle
+  else if (bottles > 0) {
+    console.log(`${bottles} bottle of beer on the wall, ${bottles} bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.\n`)
+  }
+  // 0 bottles
+  else {
+    console.log(`No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall\n`)
+  }
+}
+// }
