30a149c8072286edff0c07ff9e54d1c6e4ac1056
Katie Mayo <katie.jill@outlook.com>
2018-04-17 20:46:16 -0400

    commit

--- 06-objects/warmup.js
+++ 06-objects/warmup.js
@@ -4,3 +4,31 @@
 // 2) Write a function that rolls a 20 sided die (using the function from #1).
 //    If the number is greater than 15, roll a two six sided dice. Use the total and subtract that from the monster's health.
 //    Do this until the monster is dead.
+
+function randomNum (maxNum) {
+  return Math.ceil(Math.random() * maxNum)
+}
+
+let goblin = {
+  name: 'goblin',
+  health: 20,
+  armorClass: 12
+}
+
+function kill (monster) {
+  let attacks = 0
+  while (monster.health > 0) {
+    let attackSize = randomNum(20)
+    if (attackSize > monster.armorClass) {
+      let damage = randomNum(6) + randomNum(6)
+      monster.health -= damage
+    }
+    attacks++
+    console.log(`We're on attack #${attacks}`)
+    console.log(monster.health)
+  }
+
+  return monster.health
+}
+
+kill(goblin)
