0bf7f3027314823f1eeb6756e6c8d32b61672e59
Katie Mayo <katie.jill@outlook.com>
2018-04-17 20:45:50 -0400

    commit

--- 06-objects/exercises/monkey.js
+++ 06-objects/exercises/monkey.js
@@ -14,3 +14,30 @@ Exercise your monkeys by retrieving their properties and using their methods. Pr
 for retrieving properties (dot notation and brackets).
 
 */
+// monkey constructor
+let Monkey = function (name, species, foodsEaten) {
+  this.name = name
+  this.species = species
+  this.foodsEaten = foodsEaten
+}
+
+Monkey.prototype.eatSomething = function () {
+  let whichFood = Math.floor(Math.random() * this.foodsEaten.length)
+  // console.log(this.name + ' ate a ' + this.foodsEaten[whichFood])
+  return this.foodsEaten[whichFood]
+}
+
+Monkey.prototype.intro = function () {
+  console.log(`Hi! I am ${this.name}, a ${this.species}, and I just ate a ${this.eatSomething()}!`)
+  return `Hi! I am ${this.name}, a ${this.species}, and I just ate a ${this.eatSomething()}!`
+}
+
+let monkey1 = new Monkey('Cheryl', 'monkey', ['banana', 'plantain'])
+
+let monkey2 = new Monkey('Brad', 'chimpanzee', ['chipotle burrito', 'sandwich'])
+
+let monkey3 = new Monkey('Amber', 'gorilla', ['salad', 'grape', 'person'])
+
+monkey1.intro()
+monkey2.intro()
+monkey3.intro()
--- 06-objects/exercises/objects.js
+++ 06-objects/exercises/objects.js
@@ -8,26 +8,46 @@ Declare a variable and assign your object to it. Give your car at least four
 properties, at least one of which is a method.
 
 */
+let train = {
+    capacity: 100,
+    fare: '$2.00',
+    profitPerRider = '$1.00',
+    function daysTillService (),
+}
+
+//eg
+let reservation = {
+    checkIn: new Date();
+    checkOut: new Date();
+    userId: 1;
+    roomNum: 100;
+    paid: false;
+    settleUserAccount: function (){
+        this.paid = true
+    }
+}
 
 /*
 
 Get one of the properties from your object using dot notation
 
 */
-
+train.capacity;
+//eg
+reservation.settleUserAccount()
 /*
 
 Get one of the properties from your object using bracket notation
 
 */
-
+train[capacity]
 /*
 
 Set one of the values of one of your properties using either dot notation or
 bracket notation
 
 */
-
+train.fare = '$5.00'
 /*
 
 Call your car's method
