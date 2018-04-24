f7116964dbc570ae2dd143893d4909d95c5ec7c1
Katie Mayo <katie.jill@outlook.com>
2018-04-24 18:32:08 -0400

    commit

--- 07-intro-to-dom/exercise_solutions/js_dom_exercise/main.js
+++ 07-intro-to-dom/exercise_solutions/js_dom_exercise/main.js
@@ -2,39 +2,36 @@
 
 Making a favorites list: DOM manipulation
 
-
-- When the user clicks the submit button, take the value they've typed 
+- When the user clicks the submit button, take the value they've typed
   into the input box and add it to the list (remember: appendChild)
 
-- Also, when a new item is added to the list, clear the input box.  
+- Also, when a new item is added to the list, clear the input box.
   (Hint: the `value` property of the input box, before anyone types in it,
   is the empty string.)
 
 Bonus:
 
-When they click submit without typing anything, alert the user 
+When they click submit without typing anything, alert the user
 "you must type in a value!"
 
 */
 
-
-function addToList(list, newThing) {
-  var newThingLi = document.createElement('li');
-  var newThingText = document.createTextNode(newThing);
-  newThingLi.appendChild(newThingText);
-  list.appendChild(newThingLi);
+function addToList (list, newThing) {
+  var newThingLi = document.createElement('li')
+  var newThingText = document.createTextNode(newThing)
+  newThingLi.appendChild(newThingText)
+  list.appendChild(newThingLi)
 }
 
-window.onload = function() {
+window.onload = function () {
+  var button = document.getElementById('new-thing-button')
+  var thingList = document.getElementById('fav-list')
+  var newThingInput = document.getElementById('new-thing')
 
-  var button = document.getElementById('new-thing-button');
-  var thingList = document.getElementById('fav-list');
-  var newThingInput = document.getElementById('new-thing');
-
-  button.onclick = function(event) {
-    event.preventDefault();
-    var newThing = newThingInput.value;
-    addToList(thingList, newThing);
+  button.onclick = function (event) {
+    event.preventDefault()
+    var newThing = newThingInput.value
+    addToList(thingList, newThing)
 
     // bonus version -- instead of line 28:
 
@@ -45,9 +42,6 @@ window.onload = function() {
     //   newThingInput.value = "";
     // }
 
-    newThingInput.value = "";
-  };
+    newThingInput.value = '';
+  }
 };
-
-
-
