167fd2d504e4726c307b558654daf32d6faf6e53
Katie Mayo <katie.jill@outlook.com>
2018-05-15 18:06:55 -0400

    add

--- 13-crud-and-dbs/giphy/solution/db.js
+++ 13-crud-and-dbs/giphy/solution/db.js
@@ -7,7 +7,8 @@ const searchSchema = mongoose.Schema({
 
 mongoose.model('Search', searchSchema)
 
-mongoose.connect('mongodb://ramsay:ramsay@ds119490.mlab.com:19490/js-dc-8')
+// mongodb://ramsay:ramsay@ds119490.mlab.com:19490/js-dc-8
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds153775.mlab.com:53775/gifdata')
 const db = mongoose.connection
 
 module.exports = db
