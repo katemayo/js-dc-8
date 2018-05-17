eba914ad23ceca96fdcf167906dac4864cf15c32
Ramsay Lanier <ramsaylanier@gmail.com>
2018-05-17 11:19:02 -0400

    class 15

---
+++ 15-react-cont/exercise/solution/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Search from './components/search.js'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Search hideHomeLink={true} buttonClass="__large"/>
+      </div>
+    );
+  }
+}
+
+export default App;
